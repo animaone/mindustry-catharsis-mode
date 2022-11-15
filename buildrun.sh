beb8600e95b19cf235a9607a20bf41a33bd256e4
Anima C13 <31348553+animaone@users.noreply.github.com>
2022-11-15 09:06:06 -0300

    Update buildrun.sh

--- buildrun.sh
+++ buildrun.sh
@@ -1,2 +1,2 @@
 docker build -t mindustry_catharsis_mode .
-x11docker --gpu --sudouser --pulseaudio=host --network -- "-v $PWD/shared/:/shared/" mindustry_catharsis_mode java -jar /shared/Mindustry.jar
+x11docker --gpu --sudouser --pulseaudio=host --network -- "-v $PWD/game/:/game/ -v $PWD/home/:$HOME" mindustry_catharsis_mode java -jar /game/Mindustry.jar
